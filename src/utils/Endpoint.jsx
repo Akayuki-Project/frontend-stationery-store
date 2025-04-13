3da0ff703c96b60fbc7a6cefbbe470194a774f1f
Akayuki-Project <sanomanjirou77777@gmail.com>
2025-04-13 21:59:22 +0700

    first commit

--- src/utils/Endpoint.jsx
+++ src/utils/Endpoint.jsx
@@ -1,6 +1,8 @@
+const BaseURL = import.meta.env.VITE_API_URL;
+
 export const BASEURL = "http://localhost:4000";
 
-export const URL_SIGNIN = `${BASEURL}/api/auth/signin`;
-export const URL_PRODUCT = `${BASEURL}/api/products`;
-export const URL_TRANSACTION = `${BASEURL}/api/transactions`;
-export const URL_BANNER = `${BASEURL}/api/banners`;
+export const URL_SIGNIN = `${BaseURL}/api/auth/signin`;
+export const URL_PRODUCT = `${BaseURL}/api/products`;
+export const URL_TRANSACTION = `${BaseURL}/api/transactions`;
+export const URL_BANNER = `${BaseURL}/api/banners`;
