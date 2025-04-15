55530c60169b40e5dddb80c4f58a9d65a6b3f270
Akayuki Kobayashi <148461661+Akayuki-Project@users.noreply.github.com>
2025-04-15 20:11:28 +0700

    Update Detail.jsx

--- src/components/Product/Detail.jsx
+++ src/components/Product/Detail.jsx
@@ -114,7 +114,7 @@ const DetailProduct = () => {
             </span>
           </div>
 
-          <h3 className="text-lg font-bold mb-2">Deskripsi Produk</h3>
+          <h3 className="text-2xl font-bold mb-2">Deskripsi Produk</h3>
           <p className="text-gray-500">{product.description}</p>
         </div>
 
